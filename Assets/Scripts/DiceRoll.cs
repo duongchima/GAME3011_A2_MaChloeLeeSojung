e33a3ad16e2beda599d4b72952a513107bca0d58
chi <ma.duongchi@gmail.com>
2022-02-15 00:41:31 -0500

    created d20 roll, added difficulty settings

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class DiceRoll : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI diceText;
+    [SerializeField]
+    private Image dice;
+    public int diceNum;
+    public void RollDifficulty()
+    {
+        diceNum = Random.Range(0, 20);
+        diceText.text = diceNum.ToString();
+    }
+    void Update()
+    {
+        if(diceNum >= 0 && diceNum <= 6)
+        {
+            GameManager.Instance.UpdateDifficulty(GameManager.Difficulties.Hard);
+        }
+        else if (diceNum >= 7 && diceNum <= 13)
+        {
+            GameManager.Instance.UpdateDifficulty(GameManager.Difficulties.Medium);
+        }
+        else if (diceNum >= 14 && diceNum <= 20)
+        {
+            GameManager.Instance.UpdateDifficulty(GameManager.Difficulties.Easy);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public static Difficulties Difficulty;
+    void Start()
+    {
+       
+    }
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public enum Difficulties
+    {
+        Easy,
+        Medium,
+        Hard
+    };
+
+    public void UpdateDifficulty(Difficulties newDifficulty)
+    {
+        Difficulty = newDifficulty;
+        switch (newDifficulty)
+        {
+            case Difficulties.Easy:
+                Debug.Log("Difficulty set to easy");
+                break;
+            case Difficulties.Medium:
+                Debug.Log("Difficulty set to medium");
+                break;
+            case Difficulties.Hard:
+                Debug.Log("Difficulty set to hard");
+                break;
+        }
+    }
+}
