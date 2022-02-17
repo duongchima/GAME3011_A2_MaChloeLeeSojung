eadf37ac78646f5211fd51b87a134cec3246061b
Sojung Lee <sojung.lee2@georgebrown.ca>
2022-02-17 00:35:45 -0500

    Added Unlocking Functionality with Screwdriver

--- Assets/Scripts/Lock.cs
+++ Assets/Scripts/Lock.cs
@@ -9,18 +9,41 @@ public class Lock : MonoBehaviour
     public GameObject Screwdriver;
     public GameObject BobbyPin;
 
+    public float screwdriver_sweetSpot = 0;
+    public float bobbypin_sweetSpot = 0;
+
+    private void Start()
+    {
+        float randomAngle = Random.Range(0, 180);
+
+        screwdriver_sweetSpot = randomAngle;
+        bobbypin_sweetSpot = 0;
+    }
+
     void Update()
     {
-       if (Input.GetKey(KeyCode.D))
-       {
-            Screwdriver.transform.Rotate(Vector3.forward);
-       }
-       else if (Input.GetKey(KeyCode.A))
-       {
-            Screwdriver.transform.Rotate(-Vector3.forward);
-       }
+        if (!unlocked)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                Screwdriver.transform.Rotate(Vector3.forward, 0.1f);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                Screwdriver.transform.Rotate(-Vector3.forward, 0.1f);
+            }
 
-        BobbyPin.transform.rotation = Quaternion.Euler(0, 0, Mathf.Clamp(Input.mousePosition.x * 0.1f, 0, 180));
+            BobbyPin.transform.rotation = Quaternion.Euler(0, 0, Mathf.Clamp(Input.mousePosition.x * 0.1f, 0, 180));
+
+            if (Screwdriver.transform.rotation.eulerAngles.z > screwdriver_sweetSpot - 10 && Screwdriver.transform.rotation.eulerAngles.z < screwdriver_sweetSpot + 10)
+            {
+                unlocked = true;
+            }
+        }
+        else
+        {
+            print("unlocked");
+        }
 
     }
 }
